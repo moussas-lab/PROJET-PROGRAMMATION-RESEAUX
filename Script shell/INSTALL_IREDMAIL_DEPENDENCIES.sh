20cac82c1dd2f5cc525f71d3b821aaa3da8a1936
moussa kane <kane54517@gmail.com>
2022-04-01 02:29:39 +0000

    modif 1

---
+++ Script shell/INSTALL_IREDMAIL_DEPENDENCIES.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://github.com/iredmail/iRedMail/archive/1.5.2.tar.gz
+tar -xf 1.5.5.tar.gz
+chmod +x /home/ado/Documents/Network_Programming/Script_shell/iRedMail-1.5.2/iRedmail.sh
+./iRedMail-1.5.2/iRedmail.sh
+
