27b0110540871b99d019af0fab82579770d80b9e
moussa kane <kane54517@gmail.com>
2022-04-03 15:43:53 +0000

    modif 3

--- Script pyhton/Send_Mail.py
+++ Script pyhton/Send_Mail.py
@@ -7,3 +7,4 @@ time_exec = int(sys.argv[1])
 my_dir = os.path.dirname(sys.argv[0])
 os.system('%s %s' % (sys.executable, 
                         os.path.join(my_dir, f'Analyseur_Packet.py {time_exec} > log.txt')))
+os.system('mail -s "rapport" -A log.txt postmaster@redmail.com < log.txt')
